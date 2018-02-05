5a5105908c6fb6f519901caea370dd9d0f2704fa
VesaVilleHurskainen <17004774+VesaVilleHurskainen@users.noreply.github.com>
2018-02-05 16:58:46 +0200

    Task 3: Added script file and updated report

--- task03/scripts/task03.m
+++ task03/scripts/task03.m
@@ -1,1 +1,27 @@
-disp('Hello World!')
+clear
+
+syms m c k A real
+assumeAlso(m,'positive')
+assumeAlso(k,'positive')
+assumeAlso(c,'positive')
+
+syms x(t) f(t)
+assume(t,'real')
+assumeAlso(t>=0)
+xd = diff(x,t);
+xdd = diff(xd,t);
+
+% Set initial conditions
+x0 = A;
+xd0 = 0;
+
+% Equation of motion
+EQM = m*xdd + c*xd + k*x == 0;
+
+% Solve
+fpos = dsolve(EQM,[x(0) == x0, xd(0) == xd0])
+
+% Numerical soulution
+fsub = subs(fpos,[A,m,k,c],[0.1,1,100,0.1])
+
+fplot(fsub)
