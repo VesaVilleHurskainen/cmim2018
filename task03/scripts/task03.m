463541b407e875c8735e7a369a1cf7dfdb50d7e0
VesaVilleHurskainen <17004774+VesaVilleHurskainen@users.noreply.github.com>
2018-02-06 12:38:45 +0200

    Task 3: Finalized script

--- task03/scripts/task03.m
+++ task03/scripts/task03.m
@@ -16,14 +16,46 @@ x0 = A;
 xd0 = 0;
 
 % Equation of motion
-EQM = m*xdd + c*xd + k*x == f;
+EQM = m*xdd + c*xd + k*x == 0;
 
-% Solve
-fpos = dsolve(EQM,[x(0) == x0, xd(0) == xd0])
+% Solve response
+fpos = dsolve(EQM,[x(0) == x0, xd(0) == xd0]);
 
+% Substitute
 syms cc zeta omega real
+assumeAlso(cc>0)
+assumeAlso(zeta>0)
+assumeAlso(omega>0)
 
-fpos_rewrite = simplify(expand(subs(subs(simplify(subs(subs(fpos, 4*k*m, cc*cc), c, zeta*cc)),cc,2*sqrt(k*m)),sqrt(k/m),omega)))
+fpos_rewrite1 = simplify(subs(expand(fpos), c, zeta*cc));
+fpos_rewrite2 = simplify(subs(expand(fpos_rewrite1), 4*k*m, cc*cc));
+fpos_rewrite3 = simplify(subs(expand(fpos_rewrite2), cc, 2*sqrt(k*m)));
+fpos_rewrite4 = simplify(subs(expand(fpos_rewrite3), sqrt(k/m), omega));
 
-% Numerical solution
-fsub = subs(fpos_rewrite,[A,m,k,c],[0.1,1,100,0.1])
+disp('Symbolically derived response function:')
+pretty(fpos_rewrite4);
+
+
+% Numerical solution parameters
+tspan = [0,20];
+A_p = -1;
+omega_p = 2;
+zeta_p = 0.99;
+m_p = 1;
+
+% Force function
+f_p = 1*sin(1*t);
+
+% Solve response numerically
+k_p = omega_p*m_p;
+fsub = simplify(expand(subs(fpos_rewrite4,[A,omega,zeta,m,k,f],[A_p, omega_p, zeta_p, m_p, k_p, f_p])));
+
+% Plot response
+fplot(fsub,tspan)
+title('Butts')
+legend('datas')
+grid on
+
+filename = 'plot';
+print(filename,'-dpdf')
+% formatplot(gcf,filename)
