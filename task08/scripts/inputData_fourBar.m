56c4f4ee69aca4719231e4a067155dd339f78c8b
VesaVilleHurskainen <17004774+VesaVilleHurskainen@users.noreply.github.com>
2018-03-28 14:45:55 +0300

    Task 8: Created figures for report.

--- task08/scripts/inputData_fourBar.m
+++ task08/scripts/inputData_fourBar.m
@@ -10,7 +10,7 @@ data = struct();
 
 % Define solution parameters
 data.dt = 0.01;
-t_end = 6;
+t_end = 5;
 data.timespan = 0:data.dt:t_end;
 data.solver = 'ode45';
 data.options = odeset;
@@ -20,8 +20,8 @@ data.abstol = 1e-6;
 data.maxiter = 1000;
 
 % Define parameters for Baumgarte stabilization
-data.alpha = 1;
-data.beta = 1;
+data.alpha = 10;
+data.beta = 10;
 
 % Define numerical parameters
 data.g = [0,-9.81]';
@@ -29,51 +29,44 @@ L = 1;
 m = 1;
 
 % Define bodies and their initial positions
-type = 'slenderRod';        % Body type
-position = [0 L/2 pi/2]';   % Body initial position in global coordinates
-length = L;                 % Rod length
-mass = m;                   % Rod mass
-body1 = struct('type',type,'position',position,'length',length,'mass',mass);
+body1.type = 'slenderRod';        % Body type
+body1.position = [0 L/2 pi/2]';   % Body initial position in global coordinates
+body1.length = L;                 % Rod length
+body1.mass = m;                   % Rod mass
 
-type = 'slenderRod';
-position = [L L 0]';
-length = 2*L;
-mass = m;
-body2 = struct('type',type,'position',position,'length',length,'mass',mass);
+body2.type = 'slenderRod';
+body2.position = [L L 0]';
+body2.length = 2*L;
+body2.mass = 2*m;
 
-type = 'slenderRod';
-position = [3*L/2 L/2 -3*pi/4]';
-length = L*sqrt(2);
-mass = m;
-body3 = struct('type',type,'position',position,'length',length,'mass',mass);
+body3.type = 'slenderRod';
+body3.position = [3*L/2 L/2 -3*pi/4]';
+body3.length = L*sqrt(2);
+body3.mass = m*sqrt(2);
 
 data.bodies = [body1,body2,body3];
     
 % Define joint constraints
-joint_type = 'revolute';             % Joint type
-joint_bodies = [0,1];                % Joint bodies (numbering by order in bodies, 0 = ground)
-joint_positions = [[0;0],[-L/2;0]];  % Joint (local) positions
-joint1 = struct('type',joint_type,'bodies',joint_bodies,'positions',joint_positions);
+joint1.type = 'revolute';             % Joint type
+joint1.bodies = [0,1];                % Joint bodies (numbering by order in bodies, 0 = ground)
+joint1.positions = [[0;0],[-L/2;0]];  % Joint (local) positions
 
-joint_type = 'revolute';
-joint_bodies = [1,2];
-joint_positions = [[L/2;0],[-L;0]];
-joint2 = struct('type',joint_type,'bodies',joint_bodies,'positions',joint_positions);
+joint2.type = 'revolute';
+joint2.bodies = [1,2];
+joint2.positions = [[L/2;0],[-L;0]];
 
-joint_type = 'revolute';
-joint_bodies = [2,3];
-joint_positions = [[L;0],[-L*sqrt(2)/2;0]];
-joint3 = struct('type',joint_type,'bodies',joint_bodies,'positions',joint_positions);
+joint3.type = 'revolute';
+joint3.bodies = [2,3];
+joint3.positions = [[L;0],[-L*sqrt(2)/2;0]];
 
-joint_type = 'revolute';
-joint_bodies = [3,0];
-joint_positions = [[L*sqrt(2)/2;0],[L;0]];
-joint4 = struct('type',joint_type,'bodies',joint_bodies,'positions',joint_positions);
+joint4.type = 'revolute';
+joint4.bodies = [3,0];
+joint4.positions = [[L*sqrt(2)/2;0],[L;0]];
 
 data.joints = [joint1,joint2,joint3,joint4];
 
 % % Define time dependent constraints and their derivatives as functions
-% % At the moment, only functions of type q_i = f(t) are implemented
+% % At the moment, only functions of type q_i = f(t) are implemented,
 % const_body = 1;                     % Affected body
 % const_dof = 3;                      % Affected degree of freedom of the body
 % const_expr = @(t) (pi/2)+omega*t;   % Constraint function
@@ -81,7 +74,7 @@ data.joints = [joint1,joint2,joint3,joint4];
 % const_ddiff = @(t) 0;               % Function second time derivative
 % const1 = struct('body',const_body,'dof',const_dof,'expression',const_expr,'diff',const_diff,'ddiff',const_ddiff);
 % 
-% data.constraints = [const1];
+data.constraints = [];
 
 % END PARAMETER DEFINITIONS
 
@@ -92,4 +85,12 @@ data.joints = [joint1,joint2,joint3,joint4];
 [x,xd,t] = analyse(data);
 
 % Visualize results
-pproc_animate(x,t,data);
+% pproc_animate(x,t,data);
+
+% Plot constraint drift
+for i = 1:length(t)
+    Rc(:,i) = sum(abs(C(x(:,i),t(i),data)));
+end
+% figure
+semilogy(t,abs(Rc),'k-.','LineWidth',1);
+grid on
